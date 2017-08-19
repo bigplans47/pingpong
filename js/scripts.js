823f1de9409e7de2233fd892323b4a8a765ef456
Dan Kiss <flowfast47@gmail.com>
2017-08-18 17:13:18 -0700

    modify array to have more functionallity

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var sampleAnswer = "";
 
 var userInputMod = "";
 var userInputModCount = "";
+var answerLook ="";
 var ping = "ping";
 var pong = "pong";
 var pingpong = ping.concat(pong);
@@ -12,57 +13,73 @@ var sampleAnswerNumber = parseInt(sampleAnswer);
 
 var counter = function (userInput) {
   userInputModCount = userInput;
-  // sampleArray = int numbers[5];
-  for (i =0; i <userInputModCount; i+=1) {
-    sampleArray[i] = i;
-    alert(sampleArray);
-    // userInputModCount = sampleArray[i]
 
-    console.log();
+
+  for (i =0; i <userInputModCount; i+=1) {
+      sampleArray[i] = i+1;
   }
-  sampleArray=[];
+
+  if (sampleArray[2] === 3) {
+    sampleArray[2] = ping;
+  }
+  if (sampleArray[4] === 5) {
+    sampleArray[4] = pong;
+  }
+  if (sampleArray[14] === 15) {
+    sampleArray[14] = pingpong;
+  }
+
+  answerLook = sampleArray.toString();
+  // if (answerLook[2] === 3) {
+  //   answerlook[2] = ping;
+  // }
+
+  if (i+1 === 3) {
+    sampleArray[i] = ping;
+  } else if (i+1 % 5 === 0) {
+    sampleArray[i] = "pong";
+  }
+  else if (i+1 % 15 === 0) {
+    sampleArray[i] = "pingpong";
+  }
+
+
+    sampleArray=[];
+    console.log();
+
+  }
+  // sampleArray=[];
   // for (i= 0; i < userInputModCount; i +=) {
   //   userInputModCount = userInputModCount.append(" "+userInputModCount);
   //   <br>;
-  }
-
-
-var pingPongFunction = function (userInput) {
-
-  // var userInputMod = userInput;
-  //will come back to for function below to make program count, for now just doing a match with ifs
-  // for (i = 0; i < 5; i++) {
-  //   array[i];
-  // console.log(i);
   // }
-  if ((userInput % 15) === 0) {
-    // userInput = userInput.toString();
-    userInputMod = userInput +pingpong;
-    console.log(userInput);
-  }
-  else if (userInput % 3 === 0) {
-    userInputMod = ping;
-    // userInputMod = userInput + ping;
-  }
-  else if (userInput % 5 === 0) {
-    userInputMod = userInput + pong;
-  }
-  else {
-    userInputMod = userInput;
-  }
 
-  return;
-}
+
+// var pingPongFunction = function (userInput) {
+//   if ((userInput % 15) === 0) {
+//     userInputMod = pingpong;
+//   }
+//   else if (userInput % 3 === 0) {
+//     userInputMod = ping;
+//   }
+//   else if (userInput % 5 === 0) {
+//     userInputMod = pong;
+//   }
+//   else {
+//     userInputMod = userInput;
+//   }
+//   return;
+// }
 // frontend user interface
 $(document).ready(function(){
   $(".form-whole").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($(".userNumber").val());
     // alert(userInput);
-    pingPongFunction(userInput);
+    // pingPongFunction(userInput);
     counter(userInput);
-    $(".result").append("<li>" +userInputMod+ "</li>");
-    $(".loop").append(userInputModCount+"<br>");
+    // $(".result").append("<li>" +userInputMod+ "</li>");
+    $(".loop").prepend(answerLook+"<br>");
 
   });
 });
