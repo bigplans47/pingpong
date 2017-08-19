0740766bb398b157c8490c6a9a4ebd2bdddca131
Dan Kiss <flowfast47@gmail.com>
2017-08-18 17:57:49 -0700

    add readme and clean code

---
+++ README.md
@@ -0,0 +1,33 @@
+# _{Ping Pong by Dan Kiss}_
+
+#### _{This application converts your single number into an array with elements counting up to ##your number and for certain numbers removes the numbers and adds text instead}, {August 18th 2017}_
+
+#### By _**{Dan Kiss}**_
+
+## Description
+
+_{The array elements change so 3 is updated to and outputted as ping, 5 to pong and 15 to ##ping pong. You do not have to refresh the page to run a new entry just type it in and click ##the green button to submit it.}_
+
+## Setup/Installation Requirements
+
+{Just clone the repository and open the index.html page in a browser to use this}
+
+
+
+## Known Bugs
+
+_{None, could look nicer but no bugs}_
+
+## Support and contact details
+
+_{Dan Kiss email me as needed at flowfast47@gmail.com}_
+
+## Technologies Used
+
+_{HTML, Javascript(jquery), CSS(bootstrap)}_
+
+### License
+
+*{MIT License}*
+
+Copyright (c) 2017 **_{Dan Kiss}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,16 @@
 // backend logic
 var sampleArray = [];
-var allNumbers = "";
 var sampleAnswer = "";
-
-var userInputMod = "";
 var userInputModCount = "";
 var answerLook ="";
 var ping = "ping";
 var pong = "pong";
 var pingpong = ping.concat(pong);
-var sampleAnswerNumber = parseInt(sampleAnswer);
-
 var counter = function (userInput) {
   userInputModCount = userInput;
-
-
   for (i =0; i <userInputModCount; i+=1) {
       sampleArray[i] = i+1;
   }
-
   if (sampleArray[2] === 3) {
     sampleArray[2] = ping;
   }
@@ -28,58 +20,16 @@ var counter = function (userInput) {
   if (sampleArray[14] === 15) {
     sampleArray[14] = pingpong;
   }
-
   answerLook = sampleArray.toString();
-  // if (answerLook[2] === 3) {
-  //   answerlook[2] = ping;
-  // }
-
-  if (i+1 === 3) {
-    sampleArray[i] = ping;
-  } else if (i+1 % 5 === 0) {
-    sampleArray[i] = "pong";
-  }
-  else if (i+1 % 15 === 0) {
-    sampleArray[i] = "pingpong";
-  }
-
-
     sampleArray=[];
     console.log();
-
   }
-  // sampleArray=[];
-  // for (i= 0; i < userInputModCount; i +=) {
-  //   userInputModCount = userInputModCount.append(" "+userInputModCount);
-  //   <br>;
-  // }
-
-
-// var pingPongFunction = function (userInput) {
-//   if ((userInput % 15) === 0) {
-//     userInputMod = pingpong;
-//   }
-//   else if (userInput % 3 === 0) {
-//     userInputMod = ping;
-//   }
-//   else if (userInput % 5 === 0) {
-//     userInputMod = pong;
-//   }
-//   else {
-//     userInputMod = userInput;
-//   }
-//   return;
-// }
-// frontend user interface
+  //logic for user interface
 $(document).ready(function(){
   $(".form-whole").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($(".userNumber").val());
-    // alert(userInput);
-    // pingPongFunction(userInput);
     counter(userInput);
-    // $(".result").append("<li>" +userInputMod+ "</li>");
-    $(".loop").prepend(answerLook+"<br>");
-
+    $(".loop").prepend("<br>"+answerLook+"<br>");
   });
 });
